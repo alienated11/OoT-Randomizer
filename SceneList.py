731ec5a9105560eff2dc6d53643ecfd54b790443
alienated11 <a@a.a>
2021-12-13 16:08:41 -0800

    oops

---
+++ SceneList.py
@@ -0,0 +1,18 @@
+scene_table = [
+    {"scene_number":0x51, "name":"Hyrule Field",            "scene_data": 0x01FB8000, "collision_off":0x00008464, "mesh":None},
+    {"scene_number":0x52, "name":"Kakariko Village",        "scene_data": 0x01FF9000, "collision_off":0x00004A1C, "mesh":None},
+    {"scene_number":0x54, "name":"Zora's River",            "scene_data": 0x0204D000, "collision_off":0x00006580, "mesh":None},
+    {"scene_number":0x55, "name":"Kokiri Forest",           "scene_data": 0x0206F000, "collision_off":0x00008918, "mesh":None},
+    {"scene_number":0x56, "name":"Sacred Forest Meadow",    "scene_data": 0x020AC000, "collision_off":0x00003F4C, "mesh":None},
+    {"scene_number":0x57, "name":"Lake Hylia",              "scene_data": 0x020CB000, "collision_off":0x000055AC, "mesh":None},
+    {"scene_number":0x58, "name":"Zora's Domain",           "scene_data": 0x020F2000, "collision_off":0x00003824, "mesh":None},
+    {"scene_number":0x5A, "name":"Gerudo Valley",           "scene_data": 0x0212B000, "collision_off":0x00002128, "mesh":None},
+    {"scene_number":0x5B, "name":"Lost Woods",              "scene_data": 0x02146000, "collision_off":0x0000AC98, "mesh":None},
+    {"scene_number":0x5C, "name":"Desert Colossus",         "scene_data": 0x02186000, "collision_off":0x00004EE4, "mesh":None},
+    {"scene_number":0x5D, "name":"Gerudo Fortress",         "scene_data": 0x021AD000, "collision_off":0x00005030, "mesh":None},
+    {"scene_number":0x5F, "name":"Hyrule Castle Grounds",   "scene_data": 0x021F6000, "collision_off":0x00003CE8, "mesh":None},
+    {"scene_number":0x60, "name":"Death Mountain Trail",    "scene_data": 0x0221D000, "collision_off":0x00003D10, "mesh":None},
+    {"scene_number":0x61, "name":"Death Mountain Crater",   "scene_data": 0x02247000, "collision_off":0x000045A4, "mesh":None},
+    {"scene_number":0x62, "name":"Goron City",              "scene_data": 0x02271000, "collision_off":0x000059AC, "mesh":None},
+    {"scene_number":0x63, "name":"Lon Lon Ranch",           "scene_data": 0x029BC000, "collision_off":0x00002948, "mesh":None}
+]
