77a6e4eece44fccd50285ec84de084e8edff3456
alienated11 <a@a.a>
2022-04-22 21:40:53 -0700

    Update random skulltula reward values to be unique

--- World.py
+++ World.py
@@ -69,9 +69,10 @@ class World(object):
             self.settings.open_forest = 'closed_deku'
 
         # Randomize rewards to be 0-48
-        gs_reward_max_tokens = 6*divmod(72, 6)[0]
-        self.gs_token_rewards = [6*divmod(random.randint(1, gs_reward_max_tokens), 6)[0] for i in range(0, 5)] if settings.gs_rewards \
+        gs_reward_max_tokens = 6*divmod(48, 6)[0]
+        self.gs_token_rewards = random.sample(range(6, gs_reward_max_tokens, 6), 5) if settings.gs_rewards \
             else [10, 20, 30, 40, 50]
+        self.gs_token_rewards.sort()
 
         self.triforce_goal = settings.triforce_goal_per_world * settings.world_count
 
